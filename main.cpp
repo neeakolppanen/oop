f49adb756e0583f1e6c47055c4e047b89eb2e6e0
neeakolppanen <neea.kolpp@gmail.com>
2026-01-12 18:26:08 +0200

    Add files via upload
    tehtava 2.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,25 @@
 
 using namespace std;
 
+
+int game();
+
 int main() {
+    int arvaustenMaara = game();
+    cout << "Arvasit oikein " << arvaustenMaara << " yrityksella." << endl;
+    return 0;
+}
+
+int game() {
     srand(time(0));
     int oikea = rand() % 20 + 1;
     int arvaus = 0;
+    int laskuri = 0;
 
     while (arvaus != oikea) {
         cout << "Arvaa luku (1-20): ";
         cin >> arvaus;
+        laskuri++;
 
         if (arvaus < oikea) {
             cout << "Liian pieni!" << endl;
@@ -22,5 +33,6 @@ int main() {
         }
     }
 
-    return 0;
+    return laskuri;
 }
+
