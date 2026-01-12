026780d5b91e53f73714289759511f83bc3806c3
neeakolppanen <neea.kolpp@gmail.com>
2026-01-12 18:34:36 +0200

    Add files via upload
    tehtava 3.

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@
 
 using namespace std;
 
-
-int game();
+int game(int maxnum);
 
 int main() {
-    int arvaustenMaara = game();
+    int maxnum = 40;
+    int arvaustenMaara = game(maxnum);
     cout << "Arvasit oikein " << arvaustenMaara << " yrityksella." << endl;
     return 0;
 }
 
-int game() {
+
+int game(int maxnum) {
     srand(time(0));
-    int oikea = rand() % 20 + 1;
+    int oikea = rand() % maxnum + 1;
     int arvaus = 0;
     int laskuri = 0;
 
     while (arvaus != oikea) {
-        cout << "Arvaa luku (1-20): ";
+        cout << "Arvaa luku (1-" << maxnum << "): ";
         cin >> arvaus;
         laskuri++;
 
